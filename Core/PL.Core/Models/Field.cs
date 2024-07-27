8907124282cde5e895ce2c52f8797484e4b76c71
Роман Барковец <romanbarkovets@yandex.ru>
2024-07-27 17:11:24 +0300

    add field class, addd extensions for bmp and bmpImg objects, add drawing frame number

--- Core/PL.Core/Models/Field.cs
+++ Core/PL.Core/Models/Field.cs
@@ -1,7 +1,85 @@
-﻿namespace PL.Core.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Media.Imaging;
+using PL.Core.Constants;
+using PL.Core.Extensions;
+using Point = System.Windows.Point;
+
+namespace PL.Core.Models
 {
-	//use Singletone?
 	public class Field
 	{
+		private Bitmap _bmpImage;
+		private int _height;
+		private int _width;
+		private List<Bot> _bots;
+		private int currentFrame = 0;
+
+		public BitmapImage Map
+		{
+			get => _bmpImage.ConvertToBitmapImage();
+		}
+
+		public Field(BitmapImage bmpImg)
+		{
+			_bmpImage = bmpImg.ConvertToBmp();
+			_height = _bmpImage.Height;
+			_width = _bmpImage.Width;
+			CreateBots();
+		}
+
+		public void Update()
+		{
+			UpdateBots();
+			if (currentFrame == WorldConstants.DRAWING_FRAME)
+			{
+				currentFrame = 0;
+				Redraw();
+			}
+		}
+
+		private void Redraw()
+		{
+			var newBmp = new Bitmap(_width, _height);
+			RedrawBots(newBmp);
+			_bmpImage = newBmp;
+		}
+
+		//creation chance 1 to 8
+		private void CreateBots()
+		{
+			Random r = new Random();
+			for (int i = 0; i < _height; i++)
+			{
+				for (int j = 0; j < _width; j++)
+				{
+					if (r.Next(0, 8) == 0)
+					{
+						var bot = new Bot(new Point(j, i));
+						_bots.Add(bot);
+						_bmpImage.SetPixel(j,i,bot.Color);
+					}
+				}
+			}
+		}
+
+		private void UpdateBots()
+		{
+			foreach (var bot in _bots)
+			{
+				bot.Next();
+			}
+		}
+
+		private void RedrawBots(Bitmap bmp)
+		{
+			foreach (var bot in _bots)
+			{
+				var x = bot.Coords.X;
+				var y = bot.Coords.Y;
+				bmp.SetPixel(x,y, bot.Color);
+			}
+		}
 	}
 }
