2e1b99a2670a35bf55af082d059c031901bd1908
Роман Барковец <romanbarkovets@yandex.ru>
2024-08-08 16:11:24 +0300

    minor adds

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using PL.Core.Models;
 
 namespace project_life
 {
@@ -20,9 +24,24 @@ namespace project_life
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		private DispatcherTimer timer;
+		private TimeSpan interval;
+		private Field field;
 		public MainWindow()
 		{
 			InitializeComponent();
+			timer = new DispatcherTimer();
+			interval = new TimeSpan(0, 0, 0, 1, 0);
+			timer.Interval = interval;
+			timer.Tick += timer_Tick;
+
+			//create field
+			field = new Field(FieldImage);
+		}
+
+		private void timer_Tick(object sender, EventArgs e)
+		{
+			field.Update();
 		}
 
 		private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
@@ -78,6 +97,23 @@ namespace project_life
 		private void CurrentDaytimeCombobox_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
 		{
 			return;
+		}
+
+		public BitmapImage Convert(Image img)
+		{
+			using (var memory = new MemoryStream())
+			{
+				img.Save(memory, ImageFormat.Png);
+				memory.Position = 0;
+
+				var bitmapImage = new BitmapImage();
+				bitmapImage.BeginInit();
+				bitmapImage.StreamSource = memory;
+				bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+				bitmapImage.EndInit();
+
+				return bitmapImage;
+			}
 		}
 	}
 }
